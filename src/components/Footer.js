5d0cc6d46e02b3d34a6d9cd66542c41e8781374c
Bilal Boultif <bilalboultif@gmail.com>
2022-10-14 19:48:19 -0400

    add nvm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import react from 'react'
 
 function Footer() {
     return (
-        <div>
-           <h1>by AMAZON IT IND1</h1>
+        <div style={{backgroundImage: `url("https://www.mc-2.com/wp-content/uploads/2017/08/Zebra_CBC_2016_02-1920.jpg")`}}>
+           <h6 style={{color: "white", textAlign: "center"}}>Copyright: © 2022 AMAZON IT IND1 by Bilal Boultif</h6>
 
         </div>
     )
